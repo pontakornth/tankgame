07ace87119c8403dbee62fd07d9bd581f2a727c2
natchanon-space <natchanon.space@gmail.com>
2022-05-15 13:22:29 +0700

    implement MovementConfig using Singleton pattern

--- src/config/MovementConfig.java
+++ src/config/MovementConfig.java
@@ -1,18 +1,40 @@
 package config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
+
 public class MovementConfig {
-    private static MovementConfig playerOne;
-    private static MovementConfig playerTwo;
+    private static MovementConfig movementConfig;
+    private static Properties prop;
+    private static String CONFIG_FILE = "config/game-control.properties";
 
     private MovementConfig() {
-
+        prop = new Properties();
     }
 
-    public static MovementConfig getPlayerOne() {
-        return playerOne;
+    public static MovementConfig getInstance() {
+        if (movementConfig == null) {
+            movementConfig = new MovementConfig();
+        }
+        readProp();
+        return movementConfig;
     }
 
-    public static MovementConfig getPlayerTwo() {
-        return playerTwo;
+    public int getKeyCode(String propName) {
+        return Integer.parseInt(prop.getProperty(propName));
+    }
+
+    public static void readProp() {
+        try {
+            prop.load(new FileInputStream(CONFIG_FILE));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateProp() {
+
     }
 }
